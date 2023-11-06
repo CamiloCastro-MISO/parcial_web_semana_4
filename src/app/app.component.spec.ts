fea95b0364f76f88d685b183bca5e5d2029a17cc
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-05 23:04:52 -0500

    Punto 3 finalizado: cree un listado de 3 vehículos y verifique que la tabla se crea correctamente con tres filas más el encabezado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { VehiculoListarComponent } from './vehiculo/vehiculo-listar/vehiculo-listar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, VehiculoListarComponent],
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('parcial_semana_4');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial_semana_4 app is running!');
-  });
 });
--- src/app/vehiculo/vehiculo-listar/vehiculo-listar.component.spec.ts
+++ src/app/vehiculo/vehiculo-listar/vehiculo-listar.component.spec.ts
@@ -1,28 +1,97 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { fa, faker } from '@faker-js/faker';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { VehiculoListarComponent } from './vehiculo-listar.component';
+import { VehiculoService } from '../vehiculo.service';
+import { Vehiculo } from '../vehiculo';
 
 describe('VehiculoListarComponent', () => {
   let component: VehiculoListarComponent;
   let fixture: ComponentFixture<VehiculoListarComponent>;
+  let debug: DebugElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListarComponent ]
+      imports: [HttpClientModule],
+      declarations: [VehiculoListarComponent],
+      providers: [VehiculoService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListarComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      new Vehiculo(
+        faker.number.int(),
+        faker.person.firstName(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.number.int({ min: 1990, max: 2022 }),
+        faker.number.int({ min: 0, max: 100000 }),
+        faker.lorem.word(),
+        faker.internet.avatar()
+      ),
+      new Vehiculo(
+        faker.number.int(),
+        faker.person.firstName(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.number.int({ min: 1990, max: 2022 }),
+        faker.number.int({ min: 0, max: 100000 }),
+        faker.lorem.word(),
+        faker.internet.avatar()
+      ),
+      new Vehiculo(
+        faker.number.int(),
+        faker.person.firstName(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.number.int({ min: 1990, max: 2022 }),
+        faker.number.int({ min: 0, max: 100000 }),
+        faker.lorem.word(),
+        faker.internet.avatar()
+      )
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it("Component has a table", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+  });
+
+  it('should have an dd element ', () => {
+    const dd = debug.query(By.css('dd'));
+    const content: HTMLElement = dd.nativeElement;
+    expect(content.textContent).toContain(component.vehiculos[0].id);
+  });
+
+  it('should have a dd element for the id', () => {
+    const dds = debug.queryAll(By.css('tbody tr:first-child td dd'));
+    const content: HTMLElement = dds[0].nativeElement; // Asume que el primer dd es el ID
+    expect(content.textContent).toContain(component.vehiculos[0].id.toString());
+  });
+
+  it('should create a table with three data rows plus one header row', () => {
+    const bodyRows = debug.queryAll(By.css('tbody tr'));
+    expect(bodyRows.length).toEqual(3);
+
+    const headerRows = debug.queryAll(By.css('thead tr'));
+    expect(headerRows.length).toEqual(1);
+
+    expect(bodyRows.length + headerRows.length).toEqual(4);
+  });
+
+
 });
